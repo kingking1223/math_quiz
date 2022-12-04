ef821ff55ce3dc860957202fb9342e41dab81af6
kingking1223 <kingking1223@gmail.com>
2022-12-04 17:52:59 +0800

    Completed precalc

--- precalc/precalc.js
+++ precalc/precalc.js
@@ -1,77 +1,113 @@
-const question = document.querySelector("#question");
-const choices = Array.from(document.querySelectorAll(".choice-text"));
-const progessText = document.querySelector("#progressText");
-const scoreText = document.querySelector("#score");
-const progessBarFull = document.querySelector("#progessBarFull");
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
 
-let currentq = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
 let availableQuestions = []
+
 let questions = [
     {
-        question: "test1",
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
-        choice4: '4',
-        answer: '1'
+        question: 'What is 2 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
+        answer: 2,
     },
     {
-        question: "test2",
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
-        choice4: '4',
-        answer: '2'
+        question:
+            "The tallest building in the world is located in which city?",
+        choice1: "Dubai",
+        choice2: "New York",
+        choice3: "Shanghai",
+        choice4: "None of the above",
+        answer: 1,
     },
     {
-        question: "test3",
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
-        choice4: '4',
-        answer: '3'
+        question: "What percent of American adults believe that chocolate milk comes from brown cows?",
+        choice1: "20%",
+        choice2: "18%",
+        choice3: "7%",
+        choice4: "33%",
+        answer: 3,
     },
     {
-        question: "test4",
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
-        choice4: '4',
-        answer: '4'
+        question: "Approximately what percent of U.S. power outages are caused by squirrels?",
+        choice1: "10-20%",
+        choice2: "5-10%",
+        choice3: "15-20%",
+        choice4: "30%-40%",
+        answer: 1,
     }
-];
+]
 
-const SCORE = 100;
-const MAXQ = 4;
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
 }
 
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter > MAXQ) {
-        localStorage.setItem('mostRecentScore', score);
-        return window.location.assign('/end.html');
-    } 
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
 
-    questionCounter++;
-    progessText.innerText = `Question ${questionCounter} of ${MAXQ}`;
-    progessBarFull.style.width = `${(questionCounter / MAXQ) * 100}%`;
+        return window.location.assign('precalcend.html')
+    }
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentq = availableQuestions[questionIndex];
-    question.innerText = currentq.question;
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
 
     choices.forEach(choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentq['choice' + number];
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
     })
 
-    availableQuestions.splice(questionIndex, 1)
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
 }
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
